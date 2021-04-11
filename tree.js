2d664ed28c55b1325aedc7c3cb813b179c8f1668
Angelika <angkubicka99@gmail.com>
2021-04-11 12:28:59 +0200

    Added MathOperator class

--- tree.js
+++ tree.js
@@ -1,8 +1,8 @@
 class Node {
     constructor(value, leftChild = null, rightChild = null) {
         this.value = value; // number
-        this.leftChild = leftChild; // object
-        this.rightChild = rightChild; // object
+        this.leftChild = leftChild; // ref to object
+        this.rightChild = rightChild; // ref to object
     }
 
     hasLeftChild() {
@@ -19,7 +19,7 @@ class Node {
         if(!this.hasLeftChild()) {
             this.leftChild  = node;
         } else {
-            console.log("This node has a left child");
+            console.log("This node has already a left child");
         }
     }
 
@@ -27,7 +27,7 @@ class Node {
         if(!this.hasRightChild()) {
             this.rightChild  = node;
         } else {
-            console.log("This node has a right child");
+            console.log("This node has already a right child");
         }
     }
 
@@ -37,7 +37,7 @@ class Node {
         } else if(this.leftChild.hasLeftChild() || this.leftChild.hasRightChild()) {
             console.log("Cannot remove node with children")
         } else {
-            console.log("Deleting");
+            console.log("Deleting left child");
             this.leftChild = null;
         }
     }
@@ -48,6 +48,7 @@ class Node {
         } else if(this.Child.hasLeftChild() || this.leftChild.hasRightChild()) {
             console.log("Cannot remove node with children")
         } else {
+            console.log("Deleting right child");
             this.rightChild = null;
         }
     }
@@ -65,5 +66,71 @@ console.log(secondNode.hasLeftChild());
 firstNode.deleteRightChild();
 firstNode.deleteLeftChild();
 console.log(secondNode.leftChild.value);
-secondNode.deleteLeftChild();
-console.log(secondNode.leftChild.value);
+//secondNode.deleteLeftChild();
+
+class MathOperator {
+    constructor() {
+        this.nodesValues = [];
+    }
+
+    subtreeSearch(node) {
+        if(node.hasLeftChild()) {
+            this.subtreeSearch(node.leftChild);
+        }
+        this.nodesValues.push(node.value);
+        if(node.hasRightChild()) {
+            this.subtreeSearch(node.rightChild);
+        }
+    }
+
+    clearArray() {
+        this.nodesValues = [];
+    }
+
+    getNumberOfElements() {
+        return this.nodesValues.length;
+    }
+
+    getSumOfElements() {
+        return this.nodesValues.reduce((sum, val) => sum += val);
+    }
+
+    countAverage(sum, amount) {
+        return sum/amount;
+    }
+
+    subtreeSum(node) {
+        this.subtreeSearch(node);
+        let sum = this.getSumOfElements();
+        this.clearArray();
+        return sum;
+    }
+
+    subtreeAverage(node) {
+        this.subtreeSearch(node);
+        let sum = this.getSumOfElements();
+        let amount = this.getNumberOfElements();
+        this.clearArray();
+        return this.countAverage(sum, amount);
+    }
+
+    subtreeMedian(node) {
+        this.subtreeSearch(node);
+        const sorted = this.nodesValues.sort();
+        this.printArray(sorted)
+        let amount = this.getNumberOfElements();
+        let half = Math.floor(amount / 2);
+        let median = (!amount % 2) ? (
+            this.countAverage(sorted[half - 1] + sorted[half], 2)
+        ) : (
+            sorted[half]
+        );
+        this.clearArray();
+        return median;
+    }
+}
+
+let operator = new MathOperator;
+console.log(operator.subtreeSum(firstNode));
+console.log(operator.subtreeAverage(firstNode));
+console.log(operator.subtreeMedian(firstNode));
